22eb2f2eec2e5be69829cfc7842baf85d70a6bd0
Roman Pisani <romanp2929@gmail.com>
2025-03-15 17:59:31 -0400

    remove old audio next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name][ext]'
-      }
-    });
-    return config;
-  }
-}
+const nextConfig = {}
 
 export default nextConfig
